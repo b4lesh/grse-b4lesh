a7366df145db89c0e03f428736ec2ac987bd0265
George <peretolchin.george@gmail.com>
2020-11-26 17:28:02 +0300

    Домашняя страница перенесена

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { CommonModule } from '@angular/common';
 
 const routes: Routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(routes), CommonModule],
   exports: [RouterModule],
 })
 export class HomeRoutingModule {}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  currentUser: string | null = null;
+
+  ngOnInit(): void {
+    this.currentUser = localStorage.getItem('currentUser');
+  }
+}
